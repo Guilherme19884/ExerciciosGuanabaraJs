449d2ccc1987e40b91dd90ea3e0283d5a519c577
Guilherme Araújo <guilherme1984.araujo@gmail.com>
2022-06-26 21:21:03 -0300

    Acrescimos de exercicios

--- ex016/script.js
+++ ex016/script.js
@@ -1,1 +1,30 @@
-alert ("olá!")
+function contar(){
+    let ini = document.getElementById(`txti`)
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.length == 00 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[Erro] Faltam dados!')
+        res.innerHTML = `Impossível contar`
+    }
+    else{
+       res.innerHTML = `contando: <br>`
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+       if (i< f){
+        //contagem crescente
+            for(let c = i; c<= f; c += p){
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+       else{
+        //contagem decrescente
+            for (let c = i; c >= f; c-=p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+       }
+       res.innerHTML += `\u{1F3c1}`
+    }
+}
